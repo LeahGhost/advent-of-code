472a6dd834b93e06d9abca64f4713bde0be6b694
Leah Chandler <40885686+LeahGhost@users.noreply.github.com>
2024-12-09 22:41:30 +0000

    Day 9 part 2 🌟
    Optimising checksum calculation and processing

--- 2024/09/Go/main.go
+++ 2024/09/Go/main.go
@@ -7,29 +7,31 @@ import (
 	"strconv"
 )
 
-func computeDiskChecksum() {
-	file, err := os.Open("../input.txt")
+func readInputFile(filePath string) (string, error) {
+	file, err := os.Open(filePath)
 	if err != nil {
-		fmt.Println("Error opening file:", err)
-		return
+		return "", fmt.Errorf("error opening file: %v", err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
-	input := scanner.Text()
+	return scanner.Text(), nil
+}
+
+func computeDiskChecksum() {
+	input, err := readInputFile("../input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
 	processingFile := true
 	currentFileIndex := 0
-	diskLayout := []interface{}{} 
+	var diskLayout []interface{}
 
-	for _, sizeChar := range input {
-		blockSize, err := strconv.Atoi(string(sizeChar))
-		if err != nil {
-			fmt.Println("Error converting block size:", err)
-			return
-		}
-
+	for _, size := range input {
+		blockSize, _ := strconv.Atoi(string(size))
 		if processingFile {
 			for i := 0; i < blockSize; i++ {
 				diskLayout = append(diskLayout, currentFileIndex)
@@ -42,6 +44,7 @@ func computeDiskChecksum() {
 		}
 		processingFile = !processingFile
 	}
+
 	for i := 0; i < len(diskLayout); i++ {
 		if diskLayout[i] == "." {
 			for j := len(diskLayout) - 1; j > i; j-- {
@@ -53,15 +56,116 @@ func computeDiskChecksum() {
 			}
 		}
 	}
-	checksum := 0
+
+	totalChecksum := 0
 	for i, block := range diskLayout {
 		if block != "." {
-			checksum += block.(int) * i
+			blockVal, _ := block.(int)
+			totalChecksum += blockVal * i
 		}
 	}
-	fmt.Println("Checksum:", checksum)
+
+	fmt.Println("Checksum from Part 1:", totalChecksum)
+}
+
+func calculateOptimisedChecksum() {
+	condensedDiskMap, err := readInputFile("../input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	isFile := true
+	fileIndex := 0
+	var fullDiskMap [][]interface{}
+
+	for _, entryLength := range condensedDiskMap {
+		entryLen, _ := strconv.Atoi(string(entryLength))
+		if isFile {
+			block := make([]interface{}, entryLen)
+			for i := range block {
+				block[i] = fileIndex
+			}
+			fullDiskMap = append(fullDiskMap, block)
+			fileIndex++
+			isFile = false
+		} else {
+			if entryLen > 0 {
+				block := make([]interface{}, entryLen)
+				for i := range block {
+					block[i] = "."
+				}
+				fullDiskMap = append(fullDiskMap, block)
+			}
+			isFile = true
+		}
+	}
+
+	for i := len(fullDiskMap) - 1; i >= 0; i-- {
+		if !contains(fullDiskMap[i], ".") {
+			for j := 0; j < i; j++ {
+				if contains(fullDiskMap[j], ".") {
+					requiredSegmentLength := len(fullDiskMap[i])
+					currentSegmentLength := 0
+					segmentStartIndex := -1
+					for m := 0; m < len(fullDiskMap[j]); m++ {
+						if fullDiskMap[j][m] == "." {
+							currentSegmentLength++
+							if segmentStartIndex == -1 {
+								segmentStartIndex = m
+							}
+						} else {
+							currentSegmentLength = 0
+							segmentStartIndex = -1
+						}
+						if currentSegmentLength == requiredSegmentLength {
+							break
+						}
+					}
+
+					if currentSegmentLength == requiredSegmentLength && segmentStartIndex != -1 {
+						for m := 0; m < len(fullDiskMap[i]); m++ {
+							fullDiskMap[j][segmentStartIndex] = fullDiskMap[i][m]
+							fullDiskMap[i][m] = "."
+							segmentStartIndex++
+						}
+						break
+					}
+				}
+			}
+		}
+	}
+
+	flattenedDiskMap := flatten(fullDiskMap)
+	optimisedChecksum := 0
+	for i, block := range flattenedDiskMap {
+		if block != "." {
+			blockVal, _ := block.(int)
+			optimisedChecksum += blockVal * i
+		}
+	}
+
+	fmt.Println("Optimised Checksum from Part 2:", optimisedChecksum)
+}
+
+func contains(arr []interface{}, value interface{}) bool {
+	for _, v := range arr {
+		if v == value {
+			return true
+		}
+	}
+	return false
+}
+
+func flatten(arr [][]interface{}) []interface{} {
+	var flattened []interface{}
+	for _, subarr := range arr {
+		flattened = append(flattened, subarr...)
+	}
+	return flattened
 }
 
 func main() {
 	computeDiskChecksum()
+	calculateOptimisedChecksum()
 }
