19fa705eed3d294ef482b8ee4ea56a4a48860609
Leah Chandler <40885686+LeahGhost@users.noreply.github.com>
2024-12-12 21:14:51 +0000

    Day 12 Part 2 🔥
    Calculates the area of regions in the grid and multiplies it by the number of unique edges separating them.

--- 2024/12/Go/main.go
+++ 2024/12/Go/main.go
@@ -6,52 +6,129 @@ import (
 	"io"
 	"log"
 	"os"
+	"sort"
 )
 
-func parseInput(r io.Reader) [][]rune {
+func readGrid(input io.Reader) [][]rune {
+	scanner := bufio.NewScanner(input)
 	var grid [][]rune
-	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		grid = append(grid, []rune(scanner.Text()))
 	}
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		log.Fatalf("Error reading input: %v", err)
 	}
 	return grid
 }
 
-func isValid(grid [][]rune, p image.Point) bool {
-	return p.In(image.Rect(0, 0, len(grid[0]), len(grid)))
+func isInside(grid [][]rune, pt image.Point) bool {
+	return pt.In(image.Rect(0, 0, len(grid[0]), len(grid)))
 }
 
-func explore(grid [][]rune, p image.Point, visited map[image.Point]struct{}) (int64, int64) {
-	var area, perimeter int64
-	for _, d := range []image.Point{{0, 1}, {1, 0}, {0, -1}, {-1, 0}} {
-		np := p.Add(d)
-		if !isValid(grid, np) || grid[np.Y][np.X] != grid[p.Y][p.X] {
-			perimeter++
-		} else if _, ok := visited[np]; !ok {
-			visited[np] = struct{}{}
-			na, np := explore(grid, np, visited)
-			area += na
-			perimeter += np
+var dirs = []image.Point{
+	image.Pt(0, 1), 
+	image.Pt(1, 0),
+	image.Pt(0, -1),
+	image.Pt(-1, 0), 
+}
+
+func dfs(grid [][]rune, pt image.Point, visited map[image.Point]struct{}) (int64, int64) {
+	var area, perim int64
+	for _, d := range dirs {
+		adj := pt.Add(d)
+		if !isInside(grid, adj) || grid[adj.Y][adj.X] != grid[pt.Y][pt.X] {
+			perim++
+		} else if _, seen := visited[adj]; !seen {
+			visited[adj] = struct{}{}
+			a, p := dfs(grid, adj, visited)
+			area += a
+			perim += p
 		}
 	}
-	return area + 1, perimeter
+	return area + 1, perim
 }
 
-func calculateResult(r io.Reader) int64 {
-	grid := parseInput(r)
+func regionArea(grid [][]rune, pt image.Point, visited map[image.Point]struct{}, edges map[Side]struct{}) int64 {
+	var area int64
+	for _, d := range dirs {
+		adj := pt.Add(d)
+		if !isInside(grid, adj) || grid[adj.Y][adj.X] != grid[pt.Y][pt.X] {
+			edges[Side{d, pt}] = struct{}{}
+			continue
+		}
+		if _, seen := visited[adj]; seen {
+			continue
+		}
+		visited[adj] = struct{}{}
+		area += regionArea(grid, adj, visited, edges)
+	}
+	return area + 1
+}
+
+func countEdges(edges map[Side]struct{}) int64 {
+	var count int64
+	for _, d := range dirs {
+		lines := make(map[int][]int)
+		for e := range edges {
+			if e.dir == d {
+				axis, pos := getAxis(d, e.pt)
+				lines[axis] = append(lines[axis], pos)
+			}
+		}
+		for _, positions := range lines {
+			sort.Ints(positions)
+			count++
+			for i := 1; i < len(positions); i++ {
+				if positions[i]-positions[i-1] > 1 {
+					count++
+				}
+			}
+		}
+	}
+	return count
+}
+
+func getAxis(dir, pt image.Point) (int, int) {
+	if dir.X == 0 {
+		return pt.Y, pt.X
+	}
+	return pt.X, pt.Y
+}
+
+type Side struct {
+	dir image.Point
+	pt  image.Point
+}
+
+func part1(grid [][]rune) int64 {
 	visited := make(map[image.Point]struct{})
 	var result int64
-	for y := 0; y < len(grid); y++ {
-		for x := 0; x < len(grid[y]); x++ {
-			p := image.Pt(x, y)
-			if _, ok := visited[p]; !ok {
-				visited[p] = struct{}{}
-				area, perimeter := explore(grid, p, visited)
-				result += area * perimeter
+	for y := range grid {
+		for x := range grid[y] {
+			pt := image.Pt(x, y)
+			if _, seen := visited[pt]; !seen {
+				visited[pt] = struct{}{}
+				area, perim := dfs(grid, pt, visited)
+				result += area * perim
 			}
+		}
+	}
+	return result
+}
+
+func part2(grid [][]rune) int64 {
+	visited := make(map[image.Point]struct{})
+	var result int64
+	for y := range grid {
+		for x := range grid[y] {
+			pt := image.Pt(x, y)
+			if _, seen := visited[pt]; seen {
+				continue
+			}
+			visited[pt] = struct{}{}
+			edges := make(map[Side]struct{})
+			area := regionArea(grid, pt, visited, edges)
+			result += area * countEdges(edges)
 		}
 	}
 	return result
@@ -64,6 +141,8 @@ func main() {
 	}
 	defer file.Close()
 
-	result := calculateResult(file)
-	log.Printf("Result: %d", result)
+	grid := readGrid(file)
+
+	log.Printf("Part 1: %d", part1(grid))
+	log.Printf("Part 2: %d", part2(grid))
 }
--- 2024/12/Node/main.js
+++ 2024/12/Node/main.js
@@ -1,50 +1,135 @@
 const fs = require('fs');
 
-const parseInput = (input) => {
-  return input.split('\n').map(line => [...line]);
-};
+function readGrid(input) {
+  const grid = input.split('\n').map((line) => line.split(''));
+  return grid;
+}
 
-const isValid = (grid, p) => p.y >= 0 && p.y < grid.length && p.x >= 0 && p.x < grid[0].length;
+function isInside(grid, pt) {
+  return (
+    pt.y >= 0 && pt.y < grid.length && pt.x >= 0 && pt.x < grid[pt.y].length
+  );
+}
 
-const explore = (grid, p, visited) => {
-  let area = 0, perimeter = 0;
-  const directions = [{x: 0, y: 1}, {x: 1, y: 0}, {x: 0, y: -1}, {x: -1, y: 0}];
+const dirs = [
+  { x: 0, y: 1 },
+  { x: 1, y: 0 },
+  { x: 0, y: -1 },
+  { x: -1, y: 0 },
+];
 
-  for (let d of directions) {
-    const np = {x: p.x + d.x, y: p.y + d.y}; 
-    if (!isValid(grid, np) || grid[np.y][np.x] !== grid[p.y][p.x]) {
-      perimeter++;
-    } else if (!visited.has(`${np.x},${np.y}`)) {
-      visited.add(`${np.x},${np.y}`);
-      const [na, npPerimeter] = explore(grid, np, visited); 
-      area += na;
-      perimeter += npPerimeter;
+function dfs(grid, pt, visited) {
+  let area = 0;
+  let perim = 0;
+
+  dirs.forEach((d) => {
+    const adj = { x: pt.x + d.x, y: pt.y + d.y };
+    if (!isInside(grid, adj) || grid[adj.y][adj.x] !== grid[pt.y][pt.x]) {
+      perim++;
+    } else if (!visited.has(`${adj.x},${adj.y}`)) {
+      visited.add(`${adj.x},${adj.y}`);
+      const [a, p] = dfs(grid, adj, visited);
+      area += a;
+      perim += p;
     }
-  }
+  });
 
-  return [area + 1, perimeter];
-};
+  return [area + 1, perim];
+}
 
-const calculateResult = (input) => {
-  const grid = parseInput(input);
+function regionArea(grid, pt, visited, edges) {
+  let area = 0;
+
+  dirs.forEach((d) => {
+    const adj = { x: pt.x + d.x, y: pt.y + d.y };
+    if (!isInside(grid, adj) || grid[adj.y][adj.x] !== grid[pt.y][pt.x]) {
+      const key = `${d.x},${d.y},${pt.x},${pt.y}`;
+      edges.add(key);
+    } else if (!visited.has(`${adj.x},${adj.y}`)) {
+      visited.add(`${adj.x},${adj.y}`);
+      area += regionArea(grid, adj, visited, edges);
+    }
+  });
+
+  return area + 1;
+}
+
+function countEdges(edges) {
+  let count = 0;
+  dirs.forEach((d) => {
+    const lines = {};
+
+    edges.forEach((edge) => {
+      const [dirX, dirY, ptX, ptY] = edge.split(',').map(Number);
+      if (dirX === d.x && dirY === d.y) {
+        const axis = d.x === 0 ? ptY : ptX;
+        if (!lines[axis]) lines[axis] = [];
+        lines[axis].push(d.x === 0 ? ptX : ptY);
+      }
+    });
+
+    Object.values(lines).forEach((positions) => {
+      positions.sort((a, b) => a - b);
+      count++;
+      for (let i = 1; i < positions.length; i++) {
+        if (positions[i] - positions[i - 1] > 1) {
+          count++;
+        }
+      }
+    });
+  });
+  return count;
+}
+
+function part1(grid) {
   const visited = new Set();
   let result = 0;
 
   for (let y = 0; y < grid.length; y++) {
     for (let x = 0; x < grid[y].length; x++) {
-      const p = {x, y};
+      const pt = { x, y };
       if (!visited.has(`${x},${y}`)) {
         visited.add(`${x},${y}`);
-        const [area, perimeter] = explore(grid, p, visited);
-        result += area * perimeter;
+        const [area, perim] = dfs(grid, pt, visited);
+        result += area * perim;
       }
     }
   }
-  return result;
-};
 
-fs.readFile('../input.txt', 'utf8', (err, data) => {
-  if (err) throw err;
-  const result = calculateResult(data);
-  console.log(`Result: ${result}`);
-});
+  return result;
+}
+
+function part2(grid) {
+  const visited = new Set();
+  let result = 0;
+
+  for (let y = 0; y < grid.length; y++) {
+    for (let x = 0; x < grid[y].length; x++) {
+      const pt = { x, y };
+      if (!visited.has(`${x},${y}`)) {
+        visited.add(`${x},${y}`);
+        const edges = new Set();
+        const area = regionArea(grid, pt, visited, edges);
+        result += area * countEdges(edges);
+      }
+    }
+  }
+
+  return result;
+}
+
+function main() {
+  fs.readFile('../input.txt', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+
+    const grid = readGrid(data);
+
+    console.log(`Part 1: ${part1(grid)}`);
+    console.log(`Part 2: ${part2(grid)}`);
+  });
+}
+
+main();
